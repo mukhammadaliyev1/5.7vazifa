e781ea9187a77186a2e9980b1148f71586cba65f
mukhammadaliyev1 <muhammadnurmamadaliyev335@gmail.com>
2024-07-18 08:24:32 +0500

    b

--- js/index.js
+++ js/index.js
@@ -1,59 +1,32 @@
-import { validate,getDate ,createItem} from "./util.js";
-const form = document.getElementById('form');
-const wrapper= document.getElementById('todo-wrapper');
-const nameInput= document.getElementById('todo-name');
-const timeInput=document.getElementById("todo-time");
-const addButton=document.getElementById("add");
-const filterInput=document.getElementById("filter");
+import { validate, getDate, createItem } from "./util.js";
+const form = document.getElementById("form");
+const todoWrapper = document.querySelector("#todo-wrapper");
+const nameInput = document.getElementById("todo-name");
+const timeInput = document.getElementById("todo-time");
+const addButton = document.getElementById("add");
+const filterInput = document.getElementById("filter");
 
-
-function createItem(todo) {
-    let  isChecked = todo.status =='active' ? false :true;
-    return 
-    
-    ' <div class="todo-item">
-          <div class="left">
-            < input type="checkbox" checked = ${ isChecked  ? true : false }/>
-            <div class="info">
-              <h2>  $ {todo.name}</h2>
-              <p>$ {todo.time}</p>
-            </div>
-          </div>
-          <div class="right">
-            <span class="delete"><i class="fa-solid fa-trash"></i></span>
-
-            <span class="edit">
-              <i class="fa-solid fa-pen"></i>
-            </span>
-          </div>
-        </div>
-         ';
-}
-
-
-
-
-
-
-addButton && addButton.addEventListener('click', function(event){
+addButton &&
+  addButton.addEventListener("click", function (event) {
     event.preventDefault();
-    const isValid=validate(nameInput,timeInput);
+    const isValid = validate(nameInput, timeInput);
 
-
-    if(!isValid){
-        return;
+    if (!isValid) {
+      return;
     }
 
-const todo={
-    id:Date.now(),
-    name: nameInput.value ,
-    time:timeInput.value ,
-    status:active
-}
-let todos=getDate()
-todos.push(todo)
-localStorage.setItem('todos',JSON.stringify(todos));
-form.reset();
-const card=createItem(todo)
-wrapper.innerHTML+= card;
-})
+    const todo = {
+      id: Date.now(),
+      name: nameInput.value,
+      time: timeInput.value,
+      status: "active",
+    }
+    
+    let todos = getDate();
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    form.reset();
+
+    const card = createItem(todo);
+    todoWrapper.innerHTML += card;
+  });
--- js/util.js
+++ js/util.js
@@ -1,42 +1,46 @@
+function validate(nameInput, timeInput) {
+  if (nameInput.value.length < 3) {
+    alert("todo kamida 3 tadan kop bolishi kerak");
+    nameInput.focus();
+  }
+  if (!timeInput.value.length) {
+    alert("todo time is not defined");
+    nameInput.focus();
+    return false;
+  }
 
-function validate(nameInput, timeInput){
-
-    if(nameInput.value.length<3){
-        alert('todo kamida 3 tadan kop bolishi kerak')
-        nameInput.focus()
-    }
-    if(!timeInput.value.length){
-        alert('todo time is not defined')
-        nameInput.focus()
-        return false
-    }
-    
-    
-    return true
-    }
-
-
-
-
-function getDate(){
-    let date=[];
-    if(localStorage.getItem('todos')){
-        date=JSON.parse(localStorage.getItem('todos'))
-    }
-    return date
+  return true;
 }
 
+function getDate() {
+  let date = [];
+  if (localStorage.getItem("todos")) {
+    date = JSON.parse(localStorage.getItem("todos"));
+  }
+  return date;
+}
 
+function createItem(todo) {
+  let isChecked =todo.status == "active" ? false : true;
+  return;
+  `
+  <div class="todo-item">
+          <div class="left">
+            < input type="checkbox" checked = ${isChecked ? true : false}>
+            <div class="info">
+              <h2>  ${todo.name}</h2>
+              <p>${todo.time}</p>
+            </div>
+          </div>
+          <div class="right">
+            <span class="delete"><i class="fa-solid fa-trash"></i></span>
 
+            <span class="edit">
+              <i class="fa-solid fa-pen"></i>
+            </span>
+          </div>
+        </div>
+         `;
+}
 
-
-
-
-
-
-    
-
-
-    
-
-export{validate,getDate,createItem}
+export { validate, getDate, createItem };
